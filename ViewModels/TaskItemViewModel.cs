e1f3784f94ae2056ca92c488e2055ddd631289fa
Angel Pimentel <angelpimentelcontact.@gmail.com>
2025-05-28 17:44:56 -0400

    Add task item view model

---
+++ ViewModels/TaskItemViewModel.cs
@@ -0,0 +1,33 @@
+﻿using MvvmHelpers;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskManagerMobile.Models;
+using TaskManagerMobile.Services;
+
+namespace TaskManagerMobile.ViewModels
+{
+    public class TasksViewModel : BaseViewModel
+    {
+        public ObservableCollection<TaskItem> Tasks { get; set; } = new();
+        private DatabaseService _database;
+
+        public TasksViewModel()
+        {
+            _database = new DatabaseService();
+            LoadTasks();
+        }
+
+        public async void LoadTasks()
+        {
+            await _database.Init();
+            var tasks = await _database.GetTasksAsync();
+            Tasks.Clear();
+            foreach (var task in tasks)
+                Tasks.Add(task);
+        }
+    }
+}
